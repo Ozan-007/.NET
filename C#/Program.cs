2dc0160f66bd3e124faf47b238536e5f9c912a92
Ozan <ozan28061@gmail.com>
2022-03-14 20:10:24 +0100

    Data type conversions is explained

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+    
+// Console.WriteLine("Hello, World!");
+// System.Console.WriteLine("Hello");
+
+
+// ****************************************************
+
+//Data type conversions exercises
+
+// Calculate rectangle's perimeter and area.
+
+// System.Console.Write("Enter a value for short side : ");
+//     int shortSide = int.Parse(Console.ReadLine());
+// System.Console.Write("Enter a value for long side : ");
+//     int longSide = int.Parse(Console.ReadLine());
+
+// Console.WriteLine($"Area is {shortSide*longSide}, Perimeter is {(shortSide+longSide)*2}");
