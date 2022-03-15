249439765c5ff6c8bbaa7ec59da133739e2a0a64
Ozan <ozan28061@gmail.com>
2022-03-15 17:14:19 +0100

    Strings are explained,

--- C#/Program.cs
+++ C#/Program.cs
@@ -18,3 +18,31 @@
 //     int longSide = int.Parse(Console.ReadLine());
 
 // Console.WriteLine($"Area is {shortSide*longSide}, Perimeter is {(shortSide+longSide)*2}");
+
+
+
+// *************************************************************
+
+// Strings
+    // System.Console.Write("Name: ");
+    // string name = Console.ReadLine();
+    // System.Console.Write("SurName: ");
+    // string surname = Console.ReadLine();
+    // System.Console.Write("Age: ");
+    // int age = int.Parse(Console.ReadLine());
+
+    // string str = $"My name is {name} {surname} and I'm {age} years old.";
+    // string format_str = string.Format("My name is {0} {1} and I'm {2} years old.",name,surname,age);
+
+    // Console.WriteLine(str);
+    // Console.WriteLine(format_str);
+    
+// String Methods 
+
+
+
+
+
+
+
+
